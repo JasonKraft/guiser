469bbf60e28cadd66501bdc8f02f5241a9420be6
LimitedWard <kraftj3@rpi.edu>
2014-11-15 17:22:10 -0500

    added hello world

--- index.php
+++ index.php
@@ -1,14 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-</head>
-<body>
-	<header>
-		
-	</header>
-	<div id="content"></div>
-	<footer></footer>
-</body>
-</html>
+<?php
+echo "Hello, World!";
