b66fa07bae36a749821bb861b9033486a63b7041
LimitedWard <kraftj3@rpi.edu>
2014-11-15 19:31:43 -0500

    altered index.php
    added some css

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
-<?php
-echo "Hello, World!";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<style>
+		div {
+			font-size:30px;
+			background: rgb(0,0,0);
+			color: #fff;
+			border: 5px solid #F00;
+		}
+	</style>
+</head>
+<body>
+	<div>Hello, World!</div>
+</body>
+</html>
