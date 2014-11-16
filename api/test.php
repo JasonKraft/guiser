29bb859c8a207fa15ca80e6f2d8e47c69b857371
Christopher George <2chrisgeorge@gmail.com>
2014-11-16 01:30:01 -0500

    test case

---
+++ api/test.php
@@ -0,0 +1,24 @@
+<?php
+include "functionsv1.php";
+
+
+if(createUser("ToastyToes","HackRPI","encarc@rpi.edu")){
+	echo "You done did good with create user funct";
+}
+else{
+	echo "You done fucked up mate. This create user ";
+}
+
+echo "finding user:";
+echo findUser("ToastyToes");
+
+
+if(createPost(0,0,"yikyak","i does tests",(11/11/11, 11:11),324)){
+	echo "Post created";
+}
+else{
+	echo "Done messed up posting";
+}
+
+echo findPost(0) 
+
