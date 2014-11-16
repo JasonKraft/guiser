f592e29763789f7664c9ccc7d393c3302b3d80e8
Christian Encarnacion <encarc@rpi.edu>
2014-11-16 05:19:23 -0500

    Functions v 0.32
    Added sortByUpvote function

--- api/functionsv1.php
+++ api/functionsv1.php
@@ -168,10 +168,9 @@ function createPost($UID, $CID, $title, $content){
 	$connection = connect();
 
 	$sql = "INSERT INTO posts (UID, CID, title, content) VALUES($UID, $CID, '$title', '$content')";
-    	if(!mysqli_query($connection, $sql)){
-    		die('Error: '. mysqli_error($connection));
-    	}
-	if (mysqli_query($connection, $sql)) {
+    if(!mysqli_query($connection, $sql)){
+    	die('Error: '. mysqli_error($connection));
+    } else if {
 		$last_id = mysqli_insert_id($connection);
 	} else {
 		echo "Error!";
@@ -399,15 +398,17 @@ function toggleUpvoteComment($UID, $PCID){
 
 } 
 
-/*function sortByUpvotes($CID, $limit, $offset, $type){
+function sortByUpvotes($CID, $limit, $offset, $type){
 
 	$connection = connect();
 	if ($type == 0){
-		$query = mysqli_query($connection, "SELECT * FROM post WHERE CID = $CID ORDER BY upvotes LIMIT $limit, $offset)";
+		$query = mysqli_query($connection, "SELECT * FROM post WHERE CID = $CID AND date > DATE_SUB(CURDATE(), INTERVAL 1 DAY) ORDER BY upvotes LIMIT $limit, $offset)";
 	} else if ($type == 1){
-
+		$query = mysqli_query($connection, "SELECT * FROM post WHERE CID = $CID AND date > DATE_SUB(CURDATE(), INTERVAL 1 WEEK) ORDER BY upvotes LIMIT $limit, $offset)";
+	} else if ($type == 2){
+		$query = mysqli_query($connection, "SELECT * FROM post WHERE CID = $CID AND date > DATE_SUB(CURDATE(), INTERVAL 1 MONTH) ORDER BY upvotes LIMIT $limit, $offset)");
 	}
-}*/
+}
 
 function eraseComment($PCID){
 
@@ -456,3 +457,4 @@ function getRecentActivity($UID, $limit, $offset){
 		echo "Error: Unable to get Recent Activities";
 	}
 }
+
