f2d76ee7231833f6b4459c5dad35a089a9cc463f
LimitedWard <kraftj3@rpi.edu>
2014-11-16 01:52:21 -0500

    created register.php added API constant

--- modules/constants.php
+++ modules/constants.php
@@ -1,4 +1,5 @@
 <?php
 	define("HEADER", "../modules/header.php");
 	define("FOOTER", "../modules/footer.php");
+	define("API", "../api/functionsv1.php");
 ?>
---
+++ public/register.php
@@ -0,0 +1,13 @@
+<?php
+	include "../modules/constants.php";
+	include API;
+	include HEADER;
+?>
+
+<div id="content">
+	
+</div>
+
+<?php
+	include FOOTER;
+?>
