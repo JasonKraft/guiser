e5725b86bca13f16534ceb400fa729c56f03b93a
LimitedWard <kraftj3@rpi.edu>
2014-11-16 07:04:46 -0500

    updated constants.php to reflect new guiserAPI v1.0

--- modules/constants.php
+++ modules/constants.php
@@ -2,7 +2,7 @@
 	if (!defined('HEADER')) {
 		define("HEADER", "../modules/header.php");
 		define("FOOTER", "../modules/footer.php");
-		define("API", "../api/functionsv1.php");
+		define("API", "../api/guiserAPI.php");
 		define("LOGIN", "../api/login.php");
 		define("SESSION", "../api/session.php");
 		define("LOGOUT", "../api/logout.php");
