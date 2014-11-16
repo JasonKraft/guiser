34b364f58f758fe50c3181efe5bbd1cf1bdcec7f
LimitedWard <kraftj3@rpi.edu>
2014-11-16 06:41:39 -0500

    quick patch on duplicate constants bug

--- modules/constants.php
+++ modules/constants.php
@@ -1,9 +1,11 @@
 <?php
-	define("HEADER", "../modules/header.php");
-	define("FOOTER", "../modules/footer.php");
-	define("API", "../api/functionsv1.php");
-	define("LOGIN", "../api/login.php");
-	define("SESSION", "../api/session.php");
-	define("LOGOUT", "../api/logout.php");
-	define("INDEX", "../public/index.php");
+	if (!defined('HEADER')) {
+		define("HEADER", "../modules/header.php");
+		define("FOOTER", "../modules/footer.php");
+		define("API", "../api/functionsv1.php");
+		define("LOGIN", "../api/login.php");
+		define("SESSION", "../api/session.php");
+		define("LOGOUT", "../api/logout.php");
+		define("INDEX", "../public/index.php");
+	}
 ?>
